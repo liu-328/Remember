66bdc182e78aa75420b725d0aac53a67bdf663ec
刘诗佳 <710409068@qq.com>
2021-09-16 23:09:18 +0800

    test

--- study/函数的返回值和参数/函数内部可以通过方法修改可变参数.py
+++ study/函数的返回值和参数/函数内部可以通过方法修改可变参数.py
@@ -1,18 +1,22 @@
-def demo(num_list):
+def demo(num_list, num_dict):
 
     print("函数内部的代码")
 
     # 使用方法修改列表内的内容
     num_list.append(9)
+    num_dict.update({"name3": "小张"})
 
     print(num_list)
+    print(num_dict)
 
     print("函数执行完成")
 
 
 gl_list = [1, 2, 3]
-demo(gl_list)
+gl_dict = {"name1": "小刘", "name2": "小妮"}
+demo(gl_list, gl_dict)
 print(gl_list)
+print(gl_dict)
 
 
 
