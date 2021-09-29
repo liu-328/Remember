d29c060c27197340c497acf4f0b317e0a8464a64
刘诗佳 <710409068@qq.com>
2021-09-30 00:21:15 +0800

    test

--- hello/练习题.py
+++ hello/练习题.py
@@ -32,3 +32,65 @@ maopao(a)
 
 for i in range(len(a)):
     print("%d" % a[i])
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+# @Project  :CH_Test
+# @File     :tuzi
+# @Date     :2021/9/29 23:41
+# @Author   :Stark
+# @Email    :274808208@qq.com
+# @Software :PyCharm
+-------------------------------------------------
+"""
+# 一个兔子从第四年开始每年可以生一只兔子，20年后一共有多少只兔子？
+
+def shengtuzi(nian,longzi):
+
+    for i in range(nian):
+
+        for j in range(len(longzi)):
+
+            longzi[j] += 1
+
+            if longzi[j] >= 4:
+                longzi.append(0)
+    return longzi
+
+longzi = [0]
+nian = 4
+longzi = shengtuzi(nian, longzi)
+print(longzi)
+print(len(longzi))
+
+
+
+# 一个兔子从第四年开始每年可以生一只兔子，20年后一共有多少只兔子？
+def shengtuzi(nian,longzi):
+    for i in range(nian):  # 年份循环
+        for j in range(len(longzi)):  # 遍历笼子中的兔子
+            longzi[j] += 1  # 兔子年龄加1
+            if longzi[j] >= 4:  # 判断兔子年龄是否大于等于4，满足条件则笼子里加入一只年龄为0的小兔子
+                longzi.append(0)
+    return(longzi)
+
+longzi = [0]  # 装兔子的笼子，数值代表每只兔子的年龄
+nian = 20  # 需要计算的年份
+longzi = shengtuzi(nian, longzi)
+print(longzi)
+print(len(longzi))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
